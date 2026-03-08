ad5b4351b87b5951b6296834cf74e8bbe931db4a
Anderson Oliveira <andersonlsoliveira02@gmail.com>
2026-03-08 12:11:21 -0300

    Estrutura inical do projeto

---
+++ src/main/java/ProjetoSolucaoDescarte/solucaoDescarte/SolucaoDescarteApplication.java
@@ -0,0 +1,13 @@
+package ProjetoSolucaoDescarte.solucaoDescarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolucaoDescarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolucaoDescarteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProjetoSolucaoDescarte/solucaoDescarte/SolucaoDescarteApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetoSolucaoDescarte.solucaoDescarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolucaoDescarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
